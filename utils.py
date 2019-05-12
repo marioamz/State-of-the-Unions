b04cebe3912c3b21df0c1106f8cbf66cfa359ce7
unknown <alig89@gmail.com>
2019-05-12 11:04:08 -0500

    alix's changes

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ import utils as ut
 import importlib
 importlib.reload(ut)
 import nltk
-import collections
+from collections import Counter
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -17,13 +18,18 @@ import re
 import sys
 import spacy
 import string
-from collections import OrderedDict
+
 from os import listdir
 from os.path import isfile, join
+
+import textdistance
+from itertools import groupby
 
 stopWords = set(stopwords.words('english'))
 stopWords.add("000")
 tokenizer = RegexpTokenizer(r'\w+')
+
+lemmatizer = WordNetLemmatizer()
 
 def go():
     '''
@@ -59,47 +65,6 @@ def reading_data(PATH, filetype):
 
     return speeches
 
-def contains_multiple_words(s):   
-    """
-    function determining if the string is more than 1 word
-    
-    input:
-        s: a string
-    output:
-        True if there are more than 1 element after applying a split function. False else.
-    """
-    if len(s.split()) > 1:
-        return True
-    else:
-        return False
-
-def clean_words(speech_dict):
-    """
-    Creates a dictionary which keys are the
-    tuple (president,year), and its value a list of
-    tokenized words without StopWords.
-    
-    Inputs: Dictionary with speeches
-    Returns: Dictionary
-    """
-
-    clean_dict = {}
-    tokenizer = RegexpTokenizer(r'\w+') # we are not using it
-    stopWords = set(stopwords.words('english'))
-    for k, v in speech_dict.items():
-        if k not in clean_dict:
-            clean_dict[k]= []
-        for line in v:
-            line = str(line).lower()
-            line = line.lower().replace('"',"")
-            line = tokenizer.tokenize(line)
-            line = ' '.join(line)
-            if contains_multiple_words(line) or line not in stopWords:
-                clean_dict[k].append([line])
-                
-    clean_dict = {k: [val for sublist in v for val in sublist] for k,v in clean_dict.items()}
-    return clean_dict
-
 def chunks(dictionary):
     '''
     This function takes in a dictionary of speeches and creates
@@ -120,26 +85,73 @@ def chunks(dictionary):
         
     return new_dict
 
-def combine_counts(count_dict, use_dict):
+def contains_multiple_words(s):   
     """
-    combines counts of current and previous paragraphs
+    function determining if the string is more than 1 word
     
-    inputs:
-        count_dict: the dictionary of counted noun phrases from the current paragraph/speech
-        use_dict: the master dictionary that's tracking counts of all noun phrases across the entire corpus
-    outputs:
-        a dictionary with counts from count_dict and use_dict combined
+    input:
+        s: a string
+    output:
+        True if there are more than 1 element after applying a split function. False else.
     """
-    
-    for word in count_dict.keys():
-        if word in list(use_dict.keys()):
-            use_dict[word] += count_dict[word]
-        else:
-            use_dict[word] = count_dict[word]
-    
-    return use_dict
+    if len(s.split()) > 1:
+        return True
+    else:
+        return False
 
-def top_x(data, x):
+def nltk2wn_tag(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:          
+        return None
+    
+def lemmed(token):
+    nltk_tagged = nltk.pos_tag(nltk.word_tokenize(token))  
+    wn_tagged = map(lambda x: (x[0], nltk2wn_tag(x[1])), nltk_tagged)
+    res_words = []
+    
+    for word, tag in wn_tagged:
+        if tag is None:            
+            res_words.append(word)
+        else:
+            res_words.append(lemmatizer.lemmatize(word, tag))
+            
+    return " ".join(res_words)
+
+def clean_words(speech_dict):
+    """
+    Creates a dictionary which keys are the
+    tuple (president,year), and its value a list of
+    tokenized words without StopWords.
+    
+    Inputs: Dictionary with speeches
+    Returns: Dictionary
+    """
+    clean_dict = {}
+    tokenizer = RegexpTokenizer(r'\w+')
+    stopWords = set(stopwords.words('english'))
+    whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    for n, (k, v) in enumerate(speech_dict.items()):
+        if k not in clean_dict:
+            clean_dict[k]= []
+        for line in v:
+            line = str(line).lower()
+            line = ''.join(filter(whitelist.__contains__, line))   
+
+            if contains_multiple_words(line) or line not in stopWords:
+                lem_word = lemmed(line)
+                clean_dict[k].append([lem_word])
+        
+    clean_dict = {k: [val for sublist in v for val in sublist] for k,v in clean_dict.items()}
+    return clean_dict
+
+def count_words(data):
     """
     turns the noun phrase data into counts of noun phrases, then finds the top 1000 most stated noun phrases for the co-occurrence matrix.
     
@@ -151,20 +163,68 @@ def top_x(data, x):
         sorted: the top x most referenced noun phrases across all of the speeches.
     """
 
-    dict_use = {}
-    counter = 1
-    for n, v in enumerate(data.items()):
-        
-        name = v[0]
-        paragraph = v[1]
+    #dict_use = {}
     
-        counts = dict((str(x),paragraph.count(x)) for x in set(paragraph))
+    #for n, (name, paragraph) in enumerate(data.items()):
+    #for n, phrase in enumerate(data):
+    counts = Counter(data)        
+    #if n == 0:
+    #dict_use = counts
+    #else:
+    #dict_use = { k: dict_use.get(k, 0) + counts.get(k, 0) for k in set(dict_use) | set(dict_use) }
+    return counts
+
+def first_calc(data):
+    comparison = "america is wonderfully weird"
+    diff_calcs = []
+    for n, l in enumerate(data.values()):
+        for word in l:
+            calc = textdistance.jaccard.normalized_distance(word, comparison)
+            diff_calcs.append((word, calc))
+    return sorted(diff_calcs, key=lambda tup: tup[1])
+
+
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+             for i in range(wanted_parts) ]
+
+
+def within_calcs(data):
+    diff_calcs = []
+    for m, x in enumerate(data):
+        for n, (word, num) in enumerate(x):
+            for o, (compare_word, compare_num) in enumerate(x):
+                calc = textdistance.jaccard.normalized_distance(word,compare_word)
+                if calc < 0.16:
+                    #print(word)
+                    #print(compare_word)
+                    #print(calc)
+                    diff_calcs.append(word)
+                else:
+                    diff_calcs.append(compare_word)
+                #diff_calcs[word] = (compare_word, calc)
+        print(m)
+    return diff_calcs
+
+def lem_phrases(data):
     
-        dict_use = combine_counts(counts, dict_use)
+    diff_calcs = first_calc(data)
+    #return diff_calcs
+    #break
+    lists_split = split_list(diff_calcs, 1000)
+    print("lists split done")
+    second_diff_calcs = within_calcs(lists_split)
+    print("second diff calcs done")
     
-        counter += 1
-        
-    return dict_use, sorted(dict_use, key=dict_use.get, reverse=True)[:x]
+    return second_diff_calcs
+
+
+    
+
+def top_x(dict_use, x):
+    return sorted(dict_use, key=dict_use.get, reverse=True)[:x]
+
 
 def co_occurence_matrix(dictionary_of_speeches, dataframe=False, csv=False):
     
